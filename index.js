01bda6f5404a4d0dd4274c5ab6347df0dc6743cd
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-11-26 21:38:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function(){
+  cat.length = 0;
+  cat.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph);
+    return cats;
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob);
+    return cats;
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift();
+    return cats;
+}
+function appendCat(Broom){
+    var name_1 = [ ... cats,Broom];
+    return name_1;
+}
+function prependCat(Arnold){
+    var name_2 = [Arnold, ...cats];
+    return name_2;
+}
+function removeLastCat(){
+    var cat = cats.slice(0,cats.length-1);
+    return cat;
+}
+function removeFirstCat(){
+    var cat = cats.slice(1)
+    return cat;
+}
